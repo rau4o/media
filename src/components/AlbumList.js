663e32cf2d3caf50347219b99811e1df507afed6
rau4o <rauan.k@eventyr.pro>
2024-03-03 23:09:19 +0600

    fetch album list

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,6 +1,30 @@
+import {useFetchAlbumsQuery} from "../store";
+import ExpandablePanel from "./ExpandablePanel";
+
 function AlbumList({ user }) {
+    const { data, error, isLoading } = useFetchAlbumsQuery(user);
+    let content;
+    if (!data) {
+        content = 'Error loading albums';
+    } else {
+        content = data.map((album) => {
+            const header = <div>{album.title}</div>;
+
+            return <ExpandablePanel key={album.id} header={header}>
+                List of albums
+            </ExpandablePanel>
+        })
+    }
+
     return (
-        <div></div>
+        <div>
+            <div>
+                Albums for { user.name }
+            </div>
+            <div>
+                {content}
+            </div>
+        </div>
     )
 }
 
